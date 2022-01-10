fca180671d0471dde29eec62cf475295560c75d5
Thilina Sandaruwan <bccsthilina@gmail.com>
2022-01-10 09:18:14 +0530

    first commit

---
+++ resources/js/Components/HeaderSection.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex flex-row my-24">
+    <div class="basis-full lg:basis-3/4">
+      <p class="text-sm lg:text-3xl font-black text-[#E8D0E3]">RELEASE YOUR MUSIC</p>
+      <p class="text-5xl lg:text-9xl font-black text-[#EFDFEC]">WORLDWIDE</p>
+      <p class="text-2xl font-semibold text-[#D6CDCD]">
+        We handle everything. eTunes makes your music available for the world to
+        experience and collects streaming royalties on your behalf. Focus on
+        your music while we do the heavy lifting to make your music available to
+        the widest audience possible.
+      </p>
+      <a
+        href="#responsive-header"
+        class="
+          block
+          w-48
+          h-16
+          mt-5
+          hover:text-[#f10305]
+          text-white
+          text-center
+          py-2
+          px-5
+          text-4xl
+          font-extrabold
+          rounded-full
+          bg-gradient-to-t
+          transition-colors
+          from-[#950808]
+          to-[#FF0000]
+        "
+      >
+        Login
+      </a>
+    </div>
+    <div class="basis-full lg:basis-1/4">
+      <div style="box-shadow: 7px 7px #e0131c">
+        <img
+          src="/assets/header-section-image.jpg"
+          alt="Header Section Image"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+};
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <default-layout>
+        <header-section />
+    </default-layout>
+</template>
+
+<script>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import DefaultLayout from '@/Layouts/Default.vue';
+import HeaderSection from '@/Components/HeaderSection.vue';
+
+export default {
+    components: {
+      Head,
+      Link,
+      DefaultLayout,
+      HeaderSection,
+    },
+}
+</script>
